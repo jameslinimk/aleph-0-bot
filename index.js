b85b3b7a18e9faf7b4223ecee3daf6c44182ba5f
James Lin <63650909+jameslinimk@users.noreply.github.com>
2020-06-26 12:22:58 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,18 @@
-// Must have discord.js, package.json and package-lock.json preinstalled. **This is just the code**a
-
-
+// Must have discord.js, package.json and package-lock.json preinstalled. **This is just the code**
 
 const Discord = require('discord.js');
-
 const bot = new Discord.Client();
 
-const token = '// Your token here';
+const token = 'Insert token here';
 
+// Change this if you want
+const PREFIX = 's.';
 
-// Custom prefixes; remove if wanted.
-var PREFIX = {};
-var defaultPREFIX = ".";
-
-bot.on("ready", function(){
-    var allGuilds = bot.guilds.array();
-
-    allGuilds.forEach(element => {
-        if (!prefix[element]){
-            prefix[element] = defaultPrefix;
-        }
-    });
-});
+// Server specific vars
+var dmHelpCommand = {};
+var dmHelpReminder = {};
+var removeBotMessages = {};
+var removeBotMessagesTime = {};
 
 
 bot.on('ready', () =>{
@@ -39,8 +30,13 @@ bot.on('ready', () =>{
     console.log('');
     console.log('=======================');
     bot.user.setStatus('dnd');
-    bot.user.setActivity(' | @me for help', { type: 'PLAYING' })
+    bot.user.setActivity('pink lives matter | @me for help', { type: 'PLAYING' })
     .catch(console.error);
+
+});
+
+bot.on('guildCreate', guild => {
+    guild.owner.send(`Thanks for adding me to **${msg.guild.name}**! Use **.tutorial** or **.help** to get started!`);
 });
 
 bot.on('message', msg => {
@@ -49,7 +45,7 @@ bot.on('message', msg => {
 
     if (msg.author.bot) return;
 
-    if (msg.channel.type == "dm") {
+    if (msg.channel.type == "dm" && !lmsg.startsWith(PREFIX)) {
         var botReplies = [
             '... im a bot',
             'beep boop yes me real human.... bruh',
@@ -76,8 +72,6 @@ bot.on('message', msg => {
             'Error stupidity meter is off the charts!',
             '...',
             'tempting...',
-            '1, 2, 3, 4, who the fu** are you?',
-            'Roses are red, violets are blue, just tell me, who the fu** are you?',
             'how did you get this number?',
             'are you really that sad?',
             '**:(** <- you',
@@ -92,7 +86,7 @@ bot.on('message', msg => {
             'Im telling mommy',
             `Teacher! ${msg.author} is DMing me`,
             'Im telling my dad.',
-            '@here , we have a new winner for the **most stupid person ever**!',
+            '@here, we have a new winner for the **most stupid person ever**!',
             'I know who you are...... **Karen**',
             'I know who you are...... **the government**',
             'If id have to choose, id pick **no**',
@@ -114,160 +108,87 @@ bot.on('message', msg => {
         
         msg.author.send(botReplies[botRepliesRN]);
         return;
-    }
-
-    if (lmsg === ",setprofile"){
-        msg.delete();
-    };
+    } 
+    if (lmsg.startsWith(PREFIX) || msg.mentions.has(bot.user)){
+        const largs = msg.content.split(" ");
     
-    if (lmsg.startsWith("?warn") || lmsg.startsWith("-warn")){
-        msg.reply('do you mean **r!warn**?')
-        .then(msg => {
-            msg.delete({ timeout: 10000 });
-        });
-    };
+        let lcommand = msg.content.toLowerCase().split(" ")[0];
+        lcommand = lcommand.slice(PREFIX.length);
 
-    if (lmsg.startsWith("?kick") || lmsg.startsWith("r!kick")){
-        msg.reply('do you mean **-kick**?')
-        .then(msg => {
-            msg.delete({ timeout: 10000 });
-        });
-    };
-
-    if (lmsg.startsWith("?ban") || lmsg.startsWith("r!ban")){
-        msg.reply('do you mean **-ban**?')
-        .then(msg => {
-            msg.delete({ timeout: 10000 });
-        });
-    };
-
-    if (lmsg.startsWith("?mute") || lmsg.startsWith("r!mute")){
-        msg.reply('do you mean **-mute**?')
-        .then(msg => {
-            msg.delete({ timeout: 10000 });
-        });
-    };
-
-    if (lmsg === "i need help"){
-        msg.reply('to ask basic questions, ping a staff member. Or, create a ticket.')
-        .then(msg => {
-            msg.delete({ timeout: 10000 });
-        });
-    };
+        if (lcommand === 'test'){
+            msg.author.send('test to you!');
+        }
+        return;
+    }
 
     if (!msg.content.startsWith(PREFIX)) return;
 
     const args = msg.content.split(" ");
-
+    
     let command = msg.content.toLowerCase().split(" ")[0];
     command = command.slice(PREFIX.length);
 
-    // Commands
-    if (command === 'prefix'){
-        if (args[1] === 'update'){
-            if (autoPrefixUpdater === false){
-                msg.channel.send('Starting auto prefix updater....')
-                .then(msg => {
-                    msg.delete({ timeout: 10000 });
-                });
-                autoPrefixUpdater = true;
-                setInterval(() => {
-                    var gMembers = msg.guild.members.cache;
-                    gMembers.forEach(element => {
-                        // Prefixes
+    // Default options
+    if (!dmHelpCommand[msg.guild.id]) dmHelpCommand[msg.guild.id] = true;
+    if (!dmHelpReminder[msg.guild.id]) dmHelpReminder[msg.guild.id] = true;
+    if (!removeBotMessages[msg.guild.id]) removeBotMessages[msg.guild.id] = true;
+    if (!removeBotMessagesTime[msg.guild.id]) removeBotMessagesTime[msg.guild.id] = 10;
 
-                        if (element.roles.cache.has('724387463983792228')){
-                            if (element.user.username.startsWith('✦')) return;
-                            element.setNickname(`✦ ${element.user.username}`)
-                            .catch(error => {
-                                console.log(`Error cant change nickname of ${element.user.username}`);
-                            });
-                        };
-
-                        if (element.roles.cache.has('724387588353556612')){
-                            if (element.user.username.startsWith('✾')) return;
-                            element.setNickname(`✾ ${element.user.username}`)
-                            .catch(error => {
-                                console.log(`Error cant change nickname of ${element.user.username}`);
-                            });
-                        };
-
-                        if (element.roles.cache.has('724387500310790156')){
-                            if (element.user.username.startsWith('★')) return;
-                            if (element.roles.cache.has('724387588353556612')) return;
-                            element.setNickname(`★ ${element.user.username}`)
-                            .catch(error => {
-                                console.log(`Error cant change nickname of ${element.user.username}`);
-                            });
-                        };
-
-                        if (element.roles.cache.has('724387524755193938')){
-                            if (element.user.username.startsWith('☆')) return;
-                            if (element.roles.cache.has('724387588353556612')) return;
-                            if (element.roles.cache.has('724387500310790156')) return;
-                            element.setNickname(`☆ ${element.user.username}`)
-                            .catch(error => {
-                                console.log(`Error cant change nickname of ${element.user.username}`);
-                            });
-                        };
-
-                        // If-roles
-
-                        if (element.roles.cache.has('724387588353556612')){
-                            if (!element.roles.cache.has('724387500310790156')){
-                                let role = msg.guild.roles.cache.get('724387500310790156');
-                                element.roles.add(role);
-                            };
-                            if (!element.roles.cache.has('724387524755193938')){
-                                let role = msg.guild.roles.cache.get('724387524755193938');
-                                element.roles.add(role);
-                            };
-                        };
-                        if (element.roles.cache.has('724387500310790156')){
-                            if (!element.roles.cache.has('724387524755193938')){
-                                let role = msg.guild.roles.cache.get('724387524755193938');
-                                element.roles.add(role);
-                            };
-                        }
-                    });
-                }, 10 * 1000)
-            } else if (autoPrefixUpdater === true){
-                msg.channel.send('Auto prefix updater is already active!')
-                .then(msg => {
-                    msg.delete({ timeout: 10000 });
-                });
-                return;
-            };
-        } else if (args[1] === 'list'){
-            const prefixembed = new Discord.MessageEmbed()
-            .setColor("#7289DA")
-            .setAuthor("Connect Bot", bot.user.displayAvatarURL())
-            .setDescription(`
-__**Prefix List**__
-> \`Owner:\` ♕
-> \`Admin:\` ✾
-> \`Mod:\` ★
-> \`Helper:\` ☆`)
-            .setFooter("©️ 2020 JameslinimkYT", "https://app.zealcord.xyz/assets/Logo.png");
-            msg.channel.send(prefixembed);
-        } else if (args[1] === 'help'){
-            const helplistembed = new Discord.MessageEmbed()
-            .setColor("#7289DA")
-            .setAuthor("Connect Bot", bot.user.displayAvatarURL())
-            .setDescription(`
-__**Commands List**__
-> \`.prefix list\` > \`Lists all of the prefixes.\`
-> \`.prefix update\` > \`Updates prefixes and if roles.\``)
-            .setFooter("©️ 2020 JameslinimkYT", "https://app.zealcord.xyz/assets/Logo.png");
-            msg.channel.send(helplistembed);
-        } else{
-            msg.channel.send('Wrong args! Use `.prefix help` for more info.')
+    // Functions
+    function mReply(m){
+        if (removeBotMessages[msg.guild.id]){
+            msg.channel.send(m)
             .then(msg => {
-                msg.delete({ timeout: 10000 });
+                msg.delete({ timeout: removeBotMessagesTime * 1000 });
             });
-        }
+        } else {
+            msg.channel.send(m);
+        };
     };
-    if (command === 'search'){
+
+    // Commands
+    if (command === 'help' || msg.mentions.has(bot.user)){
+        const helpEmbed = new Discord.MessageEmbed()
+            .setColor('#0099ff')
+            .setTitle('Sphinx help command')
+            .setURL('https://docs.google.com/document/d/1dKc7qeDap2HtE-KJLnESepEl_nVgJhSyM0LEqNrBBJo/edit?usp=sharing')
+            .setAuthor('Some name', 'https://cdn.discordapp.com/app-icons/726065453591560262/8f1f0617d88e2608736f92aa8aa8fafa.png?size=64', 'https://docs.google.com/document/d/1dKc7qeDap2HtE-KJLnESepEl_nVgJhSyM0LEqNrBBJo/edit?usp=sharing')
+            .setDescription(`Preifx: **s.**`)
+            .addFields(
+                { name: 'Main commands', value: `\`s.help [<page 1-10>]\`: Shows a certain help page. (Page 1 by default)
+                \`s.tutorial\`: Shows you the basics of the bot
+                \`s.settings\`: Edits the current server settings. (Prefix, etc)
+                \`s.Info\`: Shows bot info. (Version, desc, website, etc)
+                \`s.Invite\`: Shows the bot invite link.
+                \`s.Permslist\`: Lists all of the permissions for each command.
+                \`s.Perms <@user or @role> [<perm>]\`: Gives (or lists) a user / role a permission node(s). (Only owner and users / groups with perm)`},
+                { name: 'Inline field title', value: 'Some value here', inline: true },
+                { name: 'Inline field title', value: 'Some value here', inline: true },
+            )
+            .addField('Inline field title', 'Some value here', true)
+            .setTimestamp()
+            .setFooter('Some footer text here', 'https://cdn.discordapp.com/app-icons/726065453591560262/8f1f0617d88e2608736f92aa8aa8fafa.png?size=64');
+        
+        if (dmHelpCommand[msg.guild.id]){
+            msg.author.send(helpEmbed);
+            if (removeBotMessages[msg.guild.id]){
+
+                mReply('You have mail! :mailbox:');
+
+            }
+            if (dmHelpReminder[msg.guild.id]){
+                msg.author.send('*If you want it to send it in chat other than DM, change it in settings.');
+                msg.author.send('You can also disable these messages in the settings.');
+                msg.author.send('P.S. Did you know you can execute commands here in the DMs?*');
+            }
+        } else {
+            msg.channel.send(helpEmbed);
+        };
+    } else if (command === 'settings'){
+
+    } else if (command === 'test'){
+        msg.author.send('test');
+    } else if (command === 'search'){
         if (!args[1]){
             msg.channel.send('... Search what?')
             .then(msg => {
@@ -281,11 +202,11 @@ __**Commands List**__
             .setAuthor("Connect Bot", bot.user.displayAvatarURL())
             .setURL(`https://www.google.com/search?q=${gsearch.replace(new RegExp(" ", "g"), '+')}`)
             .setTitle(`__**${msg.author.username}'s Google search**__`)
-            .setFooter("©️ 2020 JameslinimkYT", "https://app.zealcord.xyz/assets/Logo.png");
+            .setTimestamp()
+            .setFooter('Some footer text here', 'https://cdn.discordapp.com/app-icons/726065453591560262/8f1f0617d88e2608736f92aa8aa8fafa.png?size=64');
             msg.channel.send(searchembed);
         }
-    }
-    if (command === 'ytsearch'){
+    } else if (command === 'ytsearch'){
         if (!args[1]){
             msg.channel.send('... Search what?')
             .then(msg => {
@@ -299,9 +220,12 @@ __**Commands List**__
             .setAuthor("Connect Bot", bot.user.displayAvatarURL())
             .setURL(`https://www.youtube.com/search?q=${gsearch.replace(new RegExp(" ", "g"), '+')}`)
             .setTitle(`__**${msg.author.username}'s Youtube search**__`)
-            .setFooter("©️ 2020 JameslinimkYT", "https://app.zealcord.xyz/assets/Logo.png");
+            .setTimestamp()
+            .setFooter('Some footer text here', 'https://cdn.discordapp.com/app-icons/726065453591560262/8f1f0617d88e2608736f92aa8aa8fafa.png?size=64');
             msg.channel.send(searchembed);
         }
+    } else {
+        msg.channel.send('Invalid command! Do `s.help` for a list of valid commands!')
     }
 });
 
